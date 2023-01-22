61ef4a048936337eebdfc58458849b4eda8c8547
m1212e <->
2023-01-22 20:13:28 +0100

    Removed spigot logo

--- build/_app/immutable/chunks/1-a4459df3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-f4ab96ef.js";export{t as component};
--- build/_app/immutable/chunks/2-dc46d2c6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-64d6c72f.js";export{t as component};
